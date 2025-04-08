d51fd980c0470a898efc91f4d805d3564ccd4dfe
GODAOS <jmskkr@gmail.com>
2025-04-08 16:50:10 +0900

    :recycle: ?nullable 제거

--- backend/src/main/java/com/tripfriend/domain/trip/information/repository/TripInformationRepository.kt
+++ backend/src/main/java/com/tripfriend/domain/trip/information/repository/TripInformationRepository.kt
@@ -3,7 +3,7 @@ package com.tripfriend.domain.trip.information.repository
 import com.tripfriend.domain.trip.information.entity.TripInformation
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TripInformationRepository : JpaRepository<TripInformation?, Long?> {
+interface TripInformationRepository : JpaRepository<TripInformation, Long> {
     // 여행 일정 id에 따른 여행 정보 조회
-    fun findByTripScheduleId(id: Long?): List<TripInformation?>?
+    fun findByTripScheduleId(id: Long): List<TripInformation>
 }
--- backend/src/main/java/com/tripfriend/domain/trip/schedule/repository/TripScheduleRepository.kt
+++ backend/src/main/java/com/tripfriend/domain/trip/schedule/repository/TripScheduleRepository.kt
@@ -3,7 +3,7 @@ package com.tripfriend.domain.trip.schedule.repository
 import com.tripfriend.domain.trip.schedule.entity.TripSchedule
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TripScheduleRepository : JpaRepository<TripSchedule?, Long?> {
+interface TripScheduleRepository : JpaRepository<TripSchedule, Long> {
     // 특정 회원이 등록한 모든 여행 일정 조회
-    fun findByMemberId(memberId: Long?): List<TripSchedule?>?
+    fun findByMemberId(memberId: Long): List<TripSchedule>
 }
