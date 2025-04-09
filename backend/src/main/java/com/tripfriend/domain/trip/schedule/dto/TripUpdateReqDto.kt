423372fd769ae1554e9ccc94fb45909f6fc21427
GODAOS <jmskkr@gmail.com>
2025-04-09 12:19:58 +0900

    :recycle: TripScheduleReqDto 마이그레이션

--- backend/src/main/java/com/tripfriend/domain/trip/schedule/dto/TripUpdateReqDto.kt
+++ backend/src/main/java/com/tripfriend/domain/trip/schedule/dto/TripUpdateReqDto.kt
@@ -1,26 +1,18 @@
-package com.tripfriend.domain.trip.schedule.dto;
+package com.tripfriend.domain.trip.schedule.dto
+
+import com.tripfriend.domain.trip.information.dto.TripInformationUpdateReqDto
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotNull
 
 
-import com.tripfriend.domain.trip.information.dto.TripInformationUpdateReqDto;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
+data class TripUpdateReqDto(
+    @field:NotNull
+    val tripScheduleId: Long, // 여행 일정 ID
 
-import java.util.List;
+    @field:Valid
+    val scheduleUpdate: TripScheduleUpdateReqDto, // 여행 일정 수정 정보
 
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class TripUpdateReqDto {
-
-    @NotNull
-    private Long tripScheduleId;  // 여행 일정 ID
-
-    @Valid
-    private TripScheduleUpdateReqDto scheduleUpdate; // 여행 일정 수정 정보
-
-    @Valid
-    private List<TripInformationUpdateReqDto> tripInformationUpdates; // 여행 정보 수정 리스트
+    @field:Valid
+    val tripInformationUpdates: List<TripInformationUpdateReqDto>, // 여행 정보 수정 리스트
+) {
 }
