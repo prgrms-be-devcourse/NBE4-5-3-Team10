6e2cce499a028a32ea1814c957d1a23ad2560c4d
hoechanj <jhc400573@gmail.com>
2025-04-09 15:43:28 +0900

    ♻️Refactor: EventEntity 마이그레이션

--- backend/src/main/java/com/tripfriend/domain/notice/entity/Notice.kt
+++ backend/src/main/java/com/tripfriend/domain/notice/entity/Notice.kt
@@ -1,33 +1,30 @@
 package com.tripfriend.domain.notice.entity
 
-import com.tripfriend.domain.member.member.entity.Member
 import jakarta.persistence.*
 import java.time.LocalDateTime
 
-
 @Entity
 class Notice(
+
     @Column(nullable = false, length = 255)
     var title: String,
 
     @Column(nullable = false, columnDefinition = "TEXT")
     var content: String,
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    val member: Member,
-
     @Column(nullable = false, updatable = false)
     var createdAt: LocalDateTime = LocalDateTime.now(),
 
     @Column(nullable = false)
     var updatedAt: LocalDateTime = LocalDateTime.now()
+
 ) {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 
+    // 생성 시 자동 시간 세팅
     @PrePersist
     fun prePersist() {
         val now = LocalDateTime.now()
@@ -35,13 +32,17 @@ class Notice(
         updatedAt = now
     }
 
+    // 업데이트 시 시간 자동 갱신
     @PreUpdate
     fun preUpdate() {
         updatedAt = LocalDateTime.now()
     }
 
+    // 공지사항 수정 메서드
     fun update(title: String, content: String) {
         this.title = title
         this.content = content
     }
+
+
 }
