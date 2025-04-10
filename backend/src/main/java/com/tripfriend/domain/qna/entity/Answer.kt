74f5cd77107279ddae927a661df4e9f78e38f27e
hoechanj <jhc400573@gmail.com>
2025-04-10 15:07:32 +0900

    ♻️refactor: Answer 엔티티 마이그레이션

--- backend/src/main/java/com/tripfriend/domain/qna/entity/Answer.kt
+++ backend/src/main/java/com/tripfriend/domain/qna/entity/Answer.kt
@@ -1,60 +1,52 @@
-package com.tripfriend.domain.qna.entity;
+package com.tripfriend.domain.qna.entity
 
-import com.tripfriend.domain.member.member.entity.Member;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import com.tripfriend.domain.member.member.entity.Member
+import jakarta.persistence.*
+import java.time.LocalDateTime
 
 @Entity
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class Answer {
-
-    public Answer(Question question, Member member, String content, LocalDateTime createdAt) {
-        this.question = question;
-        this.member = member;
-        this.content = content;
-        this.createdAt = createdAt;
-    }
-
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
+class Answer(
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", nullable = false)
-    private Question question;
+    val question: Question,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    val member: Member,
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String content;
+    var content: String,
 
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    var createdAt: LocalDateTime = LocalDateTime.now(),
 
     @Column(nullable = false)
-    private LocalDateTime updatedAt;
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
 
     @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
+    fun prePersist() {
+        val now = LocalDateTime.now()
+        createdAt = now
+        updatedAt = now
     }
 
-    @PreUpdate
-    public void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
+    constructor(
+        question: Question,
+        member: Member,
+        content: String,
+        createdAt: LocalDateTime
+    ) : this(
+        question = question,
+        member = member,
+        content = content,
+        createdAt = createdAt,
+        updatedAt = LocalDateTime.now()
+    )
 
 }
