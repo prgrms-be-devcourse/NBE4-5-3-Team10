27fcc1863cdc4753faf00a7659617d61b2578d9d
hoechanj <jhc400573@gmail.com>
2025-04-10 15:08:18 +0900

    ♻️refactor: QuestionRepository 마이그레이션

--- backend/src/main/java/com/tripfriend/domain/qna/repository/QuestionRepository.kt
+++ backend/src/main/java/com/tripfriend/domain/qna/repository/QuestionRepository.kt
@@ -1,24 +1,20 @@
-package com.tripfriend.domain.qna.repository;
+package com.tripfriend.domain.qna.repository
 
-import com.tripfriend.domain.qna.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
+import com.tripfriend.domain.qna.entity.Question
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-
+interface QuestionRepository : JpaRepository<Question?, Long?> {
     @Query("SELECT q FROM Question q JOIN FETCH q.member")
-    List<Question> findAllWithMember();
+    fun findAllWithMember(): List<Question>
 
     @Query("SELECT q FROM Question q JOIN FETCH q.member WHERE q.id = :id")
-    Optional<Question> findByIdWithMember(@Param("id") Long id);
+    fun findByIdWithMember(@Param("id") id: Long): Optional<Question?>?
 
     @Query("SELECT q FROM Question q JOIN FETCH q.member LEFT JOIN FETCH q.answers WHERE q.id = :id")
-    Optional<Question> findByIdWithAnswers(@Param("id") Long id);
-
+    fun findByIdWithAnswers(@Param("id") id: Long): Optional<Question?>?
 }
